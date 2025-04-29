0f892b510ca496c06076b2f816175c3a459785e7
Ranjith <ranjith.kumar@olamnet.com>
2025-04-29 17:45:28 +0530

    CTA

--- blocks/cta/cta.js
+++ blocks/cta/cta.js
@@ -1,9 +1,4 @@
 export default function decorate(block) {
   /* change to Anchor  */
   console.log(block);
-  const a = document.createElement('a');
-  a.href = 'index.html';
-  block.textContent = 'Sample';
-  block.textContent = '';
-  block.append(a);
 }
